a9fd0e54150da3828436faa58379a1b599a530ce
DAKSH SAHU <dakshsahu24@gmail.com>
2024-04-21 09:53:41 +0530

    initial commit

---
+++ react_assess/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Service from './components/Service';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+    <Home/>
+    <About/>
+    <Service/>
+    <Contact/>
+    </div>
+  );
+}
+
+export default App;
